7cfec5ac6fa4efaef8b7c59fc9879d5c337a56b9
DimitarDavidov <dimitardavidov5@gmail.com>
2024-03-31 02:47:03 +0200

    added login

--- GameFinder/src/app/user/login/login.component.ts
+++ GameFinder/src/app/user/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor(private auth: AngularFireAuth, private router: Router) { }
+
+  loginUser(): void {
+    this.auth.signInWithEmailAndPassword(this.email, this.password)
+      .then((userCredential) => {
+        console.log('User logged in successfully:', userCredential.user);
+        this.router.navigate(['/']);
+      })
+      .catch((error) => {
+        console.error('Login failed:', error.message);
+        alert('Login failed: ' + error.message);
+      });
+  }
 }
